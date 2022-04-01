28a6e1a5f68431309d71da4ba578502c3cbb98be
Dilshod070 <36330741+Dilshod070@users.noreply.github.com>
2022-04-01 20:02:00 +0300

    ping version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(fname):
 
 setup(
     name="splyne",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
